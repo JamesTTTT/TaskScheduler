4ae3ba0d60d41b042e96857189a82451f5f700ea
James Taylor <jata20@student.bth.se>
2022-09-01 10:51:08 +0100

    improved the add task form. it is now necessary to click the add new task button to display the form

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,19 +1,44 @@
 import React from 'react'
 import {BiAddToQueue}from 'react-icons/bi'
+import {AiOutlineClose} from 'react-icons/ai'
+import { useState } from 'react'
+
 const AddTask = () => {
+
+  const [showAdd, setShowAdd] = useState(false);
+
+  const displayAdd = () =>{
+    if (showAdd) {
+      setShowAdd(false)
+    } else {
+      setShowAdd(true)
+    }
+  }
+  
   return (
-    <div>
-        <div className='p-4'>
-            <button 
-            className='flex overflow-hidden text-white font-bold bg-blue-500 p-2 rounded
-            whitespace-nowrap transition ease-in-out delay-50 hover:bg-indigo-500'>
-                <span className='inline-flex'>Add New Task</span>
-                <span className='inline-flex text-xl mx-1'><BiAddToQueue/></span>
+    <>
+      <div className='p-4'>
+          <button
+          onClick={displayAdd}
+          className='flex overflow-hidden text-white font-bold bg-blue-500 p-2 rounded
+          whitespace-nowrap transition ease-in-out delay-50 hover:bg-indigo-500'>
+              <span className='inline-flex'>Add New Task</span>
+              <span className='inline-flex text-xl mx-1'><BiAddToQueue/></span>
+          </button>
+      </div>
+
+    {showAdd ? (
+        <div className='p-3'>
+          <form className='bg-slate-100 flex flex-col w-80 items-center rounded-md p-4'>
+            <button
+              onClick={displayAdd} 
+              className='m-1 p-1 bg-red-500 text-white rounded-xl'
+              >
+              <AiOutlineClose/>
             </button>
-        </div>
-        <div>
-          <form>
-            <label>Enter Task Title</label>
+            <h1 className='text-l'>Create Task</h1>
+            <div>
+            <label>Enter Title</label>
             <input
              type="text"
              name="title"
@@ -22,7 +47,10 @@ const AddTask = () => {
              text-sm rounded-lg block p-2.5 w-72'
              required
              />
-            <label>Enter Task Title</label>
+            </div>
+
+            <div>
+            <label>Enter Description</label>
             <textarea
              type="text"
              name="description"
@@ -32,6 +60,9 @@ const AddTask = () => {
              text-sm rounded-lg block p-2.5 w-72'
              required
              />
+            </div>
+
+            <div>
             <label>Select a category</label>
             <select 
             className='bg-gray-50 border border-gray-300 text-gray-900
@@ -43,11 +74,19 @@ const AddTask = () => {
               <option value="opel">Opel</option>
               <option value="audi">Audi</option>
             </select>
-            <submit/>
+            </div>
+
+            <div className='mt-4'>
+            <button 
+            className='p-2 bg-blue-500 text-white rounded
+            transition ease-in-out delay-50 hover:bg-indigo-500 w-32'>
+              Save Task
+            </button>
+            </div>
           </form>
         </div>
-
-    </div>
+        ) : (" ")}
+      </>
   )
 }
 
