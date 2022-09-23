650031e999f1dc1546f7c7778074d6f0483c3914
James Taylor <jata20@student.bth.se>
2022-09-23 10:43:25 +0100

    Added buttons in footer for export

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,16 +3,12 @@ import React from 'react'
 const Footer = () => {
   return (
     <div>
-      <div className='bg-blue-500 w-full h-20 flex justify-center items-center text-white flex-col'>
-        <h1 className='text-3xl font-bold'>
-            Footer
-        </h1>
-        <p>Task Scheduler Application all rights reserved 2022 &reg;</p>
-        {/* <ul>
-          <li>Coffee</li>
-          <li>Tea</li>
-          <li>Milk</li>
-        </ul> */}
+      <div className='bg-blue-500 w-full flex justify-center items-center text-white flex-col'>
+        <p className='text-xl p-2'>Export all tasks</p>
+        <div className=' p-3 flex flex-row justify-between'>
+          <button className='mr-5 p-2 bg-blue-700 rounded-xl hover:bg-blue-800 transition-colors'>Export CSV</button>
+          <button className='ml-5 p-2 bg-blue-700 rounded-xl hover:bg-blue-800 transition-colors'>Export PDF</button>
+        </div>
       </div>
     </div>
   )
