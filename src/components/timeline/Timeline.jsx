8ee562ee157268d2ecf33fb1dd7ee7b1c9b05ffc
James Taylor <jata20@student.bth.se>
2022-09-26 20:59:56 +0100

    radio buttons working

--- src/components/timeline/Timeline.jsx
+++ src/components/timeline/Timeline.jsx
@@ -1,39 +1,55 @@
 import React from 'react'
+import { useState } from 'react';
 import TimelineDaily from './TimelineDaily';
+import TimelineHourly from './TimelineHourly';
+
 
 const Timeline = ({loadedTasks}) => {
 
+    const [isHourly, setIsHourly] = useState(false)
+
+    const handleRadio = (bool) => {
+      setIsHourly(bool)
+    }
+
     return (
       <>
-        <h1 className='text-center text-2xl'>Timeline</h1>
+        <h1 className='text-center text-2xl pb-2 pt-5'>Timeline</h1>
         <div 
-          className='bg-slate-700 text-lg flex justify-center py-1
+          className='bg-slate-700 text-lg flex justify-center py-1 pb-2
           text-white font-light outline outline-blue-500 outline-1'
           >
-            <div className='px-1 flex flex-col'>
+            <div className='px-3 flex flex-col'>
               <label>Daily</label>
               <input
-                className='bg-gray-100'
                 type="radio" 
-                name="timeline" 
-                checked
+                name="timeline"
+                onClick={()=>{handleRadio(false)}}
               />
 
             </div>
-            <div className='px-1 flex flex-col'> 
+            <div className='px-3 flex flex-col'> 
               <label>Hourly</label>
               <input
-                className='bg-gray-100'
                 type="radio" 
                 name="timeline" 
-                checked
+                onClick={()=>{handleRadio(true)}}
               />
             </div>
         </div>
 
-        <div>
-          <TimelineDaily loadedTasks={loadedTasks}/>
-        </div>
+        {isHourly
+        ?(
+          <div>
+            <TimelineHourly loadedTasks={loadedTasks}/>
+          </div>
+        )
+        : (
+          <div>
+            <TimelineDaily loadedTasks={loadedTasks}/>
+          </div>
+        )
+        }
       </>
     )
 }
